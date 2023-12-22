2bf335a1354cc3291cd51f945e2c16835c5fee5c
mamun-prodhan <mamunpr9@gmail.com>
2023-12-22 17:27:49 +0600

    login page responsive completed

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -6,10 +6,12 @@ const Login = () => {
       <div className="flex justify-center items-center min-h-screen bg-indigo-500">
         <div className="w-72 md:w-96 lg:w-96 px-6 py-10 shadow-lg bg-white rounded-lg">
           <div className="flex items-center justify-center gap-2">
-            <h1 className="text-4xl text-center font-semibold">Login</h1>
-            <FaUser className="text-2xl" />
+            <h1 className="text-2xl md:text-3xl lg:text-4xl text-center font-semibold">
+              Login
+            </h1>
+            <FaUser className="text-xl md:text-2xl" />
           </div>
-          <hr className="my-6" />
+          <hr className="my-4 md:my-6" />
           <form>
             <div className="mt-3">
               <label htmlFor="username" className="block text-base mb-2">
@@ -19,7 +21,7 @@ const Login = () => {
                 type="text"
                 id="username"
                 name="username"
-                className="border rounded-lg w-full text-base px-3 py-3 focus:outline-none focus:ring-1 focus:border-gray-400"
+                className="border rounded-lg w-full text-base px-2 md:px-3 py-2 md:py-3 focus:outline-none focus:ring-1 focus:border-gray-400"
                 placeholder="Enter Username"
               />
             </div>
@@ -31,14 +33,14 @@ const Login = () => {
                 type="password"
                 id="password"
                 name="password"
-                className="border rounded-lg w-full text-base px-3 py-3 focus:outline-none focus:ring-1 focus:border-gray-400"
+                className="border rounded-lg w-full text-base px-2 md:px-3 py-2 md:py-3 focus:outline-none focus:ring-1 focus:border-gray-400"
                 placeholder="Enter Password"
               />
             </div>
             <div className="mt-5">
               <button
                 type="submit"
-                className="border-2 border-indigo-700 bg-indigo-700 text-white py-3 w-full rounded-md hover:bg-transparent hover:text-indigo-700 font-semibold transition-all duration-300"
+                className="border-2 border-indigo-700 bg-indigo-700 text-white py-2 md:py-3 w-full rounded-md hover:bg-transparent hover:text-indigo-700 font-semibold transition-all duration-300"
               >
                 Login
               </button>
