04c4b0489960a970ce8c0aa80115795007b1b3ee
mamun-prodhan <mamunpr9@gmail.com>
2023-12-23 11:10:53 +0600

    authentication completed

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,17 @@
 const Home = () => {
+  const logout = () => {
+    localStorage.removeItem("token");
+    setTimeout(() => {
+      window.location.reload();
+    }, 100);
+  };
   return (
     <div>
       <h2>This is home page</h2>
+      <button onClick={logout} className="btn">
+        {" "}
+        Logout
+      </button>
     </div>
   );
 };
