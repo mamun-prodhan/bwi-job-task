9dde413cd95ee20e741bb31267fbac6039fe810d
mamun-prodhan <mamunpr9@gmail.com>
2023-12-23 17:28:17 +0600

    search and filter implemented

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -5,6 +5,7 @@ const Home = () => {
   const [products, setProducts] = useState([]);
   const [filteredData, setFilteredData] = useState([]);
   const [search, setSearch] = useState("");
+  const [priceBar, setPriceBar] = useState(2000);
   const [loading, setLoading] = useState(true);
   // console.log("loading state", loading, "and products data", products.length);
 
@@ -17,7 +18,7 @@ const Home = () => {
           throw new Error(`Error Status:${response.status}`);
         }
         const data = await response.json();
-        setProducts(data.products);
+        setProducts(data.products), setFilteredData(data.products);
       } catch (error) {
         console.log(error);
       } finally {
@@ -36,6 +37,21 @@ const Home = () => {
     }, 100);
   };
 
+  const handleSearch = () => {
+    console.log("handle search");
+    const filtered = products.filter((item) =>
+      item.title.toLowerCase().includes(search.toLowerCase())
+    );
+    setFilteredData(filtered);
+  };
+
+  const handlePrice = () => {
+    const filtered = products.filter(
+      (item) => parseInt(item.price, 10) <= parseInt(priceBar, 10)
+    );
+    setFilteredData(filtered);
+  };
+
   if (loading) {
     return (
       <p className="text-5xl text-red-600 font-bold text-center my-20">
@@ -45,11 +61,45 @@ const Home = () => {
   }
 
   return (
-    <div className="max-w-7xl mx-auto">
+    <div className="max-w-7xl mx-auto mt-20">
       {/* header div */}
       <div>
-        <h2>This is home page</h2>
-        <h2 className="">Total Products {products.length}</h2>
+        {/* search bar */}
+        <div>
+          <input
+            className="border py-2 px-3 rounded-s-lg"
+            onChange={(e) => setSearch(e.target.value)}
+            type="text"
+            value={search}
+            placeholder="Search by title..."
+          />
+          <button
+            className="border py-2 px-3 text-white font-semibold rounded-e-lg bg-indigo-600 hover:bg-indigo-700 duration-300"
+            onClick={handleSearch}
+          >
+            Search
+          </button>
+        </div>
+        {/* price bar */}
+        <div>
+          <p className="text-xl font-semibold mt-5">Price</p>
+          <p>${priceBar}</p>
+          <input
+            type="range"
+            name="price"
+            min={13}
+            max={2000}
+            value={priceBar}
+            onChange={(e) => setPriceBar(e.target.value)}
+            id="price"
+          />
+          <button
+            onClick={handlePrice}
+            className="border py-2 px-3 text-white font-semibold rounded-e-lg bg-indigo-600 hover:bg-indigo-700 duration-300"
+          >
+            Filter By Price
+          </button>
+        </div>
         <div className="text-end">
           <button
             onClick={logout}
@@ -59,9 +109,17 @@ const Home = () => {
           </button>
         </div>
       </div>
-      {/* cart div */}
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 px-4 my-5 md:my-10">
-        {products.map((item) => (
+      {/* if no data found */}
+      {!filteredData.length && (
+        <div>
+          <h2 className="text-5xl font-bold text-center text-red-500 my-20">
+            No data found
+          </h2>
+        </div>
+      )}
+      {/* card div */}
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 px-4 lg:px-0 my-5 md:my-10">
+        {filteredData.map((item) => (
           <ProductCard key={item.id} item={item}></ProductCard>
         ))}
       </div>
