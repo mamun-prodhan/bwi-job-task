64cf9d6fb0d407236980066b5de35a353279ea9b
mamun-prodhan <mamunpr9@gmail.com>
2023-12-23 15:13:26 +0600

    home page card completd

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,17 +1,70 @@
+import { useEffect, useState } from "react";
+import ProductCard from "../ProductCard/ProductCard";
+
 const Home = () => {
+  const [products, setProducts] = useState([]);
+  const [filteredData, setFilteredData] = useState([]);
+  const [search, setSearch] = useState("");
+  const [loading, setLoading] = useState(true);
+  // console.log("loading state", loading, "and products data", products.length);
+
+  // fetching data from api
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("https://dummyjson.com/products");
+        if (!response.ok) {
+          throw new Error(`Error Status:${response.status}`);
+        }
+        const data = await response.json();
+        setProducts(data.products);
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
+
+  console.log(products);
+
   const logout = () => {
     localStorage.removeItem("token");
     setTimeout(() => {
       window.location.reload();
     }, 100);
   };
+
+  if (loading) {
+    return (
+      <p className="text-5xl text-red-600 font-bold text-center my-20">
+        Loading..............
+      </p>
+    );
+  }
+
   return (
-    <div>
-      <h2>This is home page</h2>
-      <button onClick={logout} className="btn">
-        {" "}
-        Logout
-      </button>
+    <div className="max-w-7xl mx-auto">
+      {/* header div */}
+      <div>
+        <h2>This is home page</h2>
+        <h2 className="">Total Products {products.length}</h2>
+        <div className="text-end">
+          <button
+            onClick={logout}
+            className="border-2 border-indigo-700 bg-indigo-700 text-white px-6 py-2  rounded-md hover:bg-transparent hover:text-indigo-700 font-semibold transition-all duration-300"
+          >
+            Logout
+          </button>
+        </div>
+      </div>
+      {/* cart div */}
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 px-4 my-5 md:my-10">
+        {products.map((item) => (
+          <ProductCard key={item.id} item={item}></ProductCard>
+        ))}
+      </div>
     </div>
   );
 };
---
+++ src/components/ProductCard/ProductCard.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const ProductCard = ({ item }) => {
+  return (
+    <div className="p-4 border rounded-lg">
+      <div
+        className="h-48 bg-cover bg-center rounded-lg border"
+        style={{ backgroundImage: `url(${item.thumbnail})` }}
+      ></div>
+      <div className="mt-3 space-y-2">
+        <h3 className="text-2xl font-semibold">{item.title}</h3>
+        <div className="flex items-center gap-5">
+          <p className="text-xl">Price: ${item.price}</p>{" "}
+          <p className="bg-red-200 px-2 rounded">
+            {item.discountPercentage}% OFF
+          </p>
+        </div>
+        <p className="text-sm capitalize text-gray-500">
+          {item.brand} | {item.category}
+        </p>
+        <button className="border border-indigo-700 bg-indigo-700 text-white px-6 py-2  rounded-md hover:bg-transparent hover:text-indigo-700 font-semibold transition-all duration-300">
+          Add To Cart
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default ProductCard;
